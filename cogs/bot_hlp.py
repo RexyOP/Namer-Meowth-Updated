b0fe6285f3c7566ed444706e3dd779dd7c16c3e6
fairytalentedtale <fairytalentedtales@gmail.com>
2026-05-08 21:00:19 +0530

    Update bot_hlp.py

--- cogs/bot_hlp.py
+++ cogs/bot_hlp.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 from config import EMBED_COLOR, BOT_PREFIX
 
-class Help(commands.Cog):
+class BotHelp(commands.Cog):
     """Help and information commands"""
 
     def __init__(self, bot):
